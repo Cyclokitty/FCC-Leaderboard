d716df02bfc9375b550caeef5f12850f2c00accc
Laura Vingada <laura@vingada.com>
2017-03-01 19:56:27 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import './App.css';
+
+const App = () => {
+    return (
+      <div className='App'>
+        <Header />
+        <Footer />
+      </div>
+
+    );
+};
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <h5>made with pride by <a href="https://github.com/Cyclokitty" target='_blank'><span className='linkName'>Laura Veee</span></a> &copy; 2017</h5>
+    </div>
+  );
+};
+
+
+
+
+export default Footer;
