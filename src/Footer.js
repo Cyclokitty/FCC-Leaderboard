131cecc44f8541fc1092c780b18dcdc1386ade94
Laura Vingada <laura@vingada.com>
2017-03-06 14:09:19 -0500

    updated footer and package.json

--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <div className='Footer'>
       <h5>made with pride by <a href="https://github.com/Cyclokitty" target='_blank'><span className='linkName'>Laura Veee</span></a> &copy; 2017</h5>
+      <h5>Source code for this project is available on <a href='https://github.com/Cyclokitty/FCC-Leaderboard' target='_blank'>Github</a></h5>
     </div>
   );
 };
